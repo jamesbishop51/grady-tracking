a33f4a23557a7a989cad9b84af31e8ea94281f95
james bishop <s00187350@mail.itsligo.ie>
2023-01-16 10:45:27 +0000

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gradys.tracking',
+  appName: 'grady-tracking',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    url: "http://100.92.238.79:5173",
+    // url: "http://192.168.1.111:5173",
+    cleartext: true
+  },
+};
+
+export default config;
