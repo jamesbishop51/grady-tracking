12ba855644b18d6a1e429cbb89d7e33863dbcfef
james bishop <s00187350@mail.itsligo.ie>
2023-01-24 12:42:28 +0000

    Set up vite layouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <main class="pt-4 pb-8">
+<div >
+  <RouterView />
+</div>
+  </main>
+  <footer>
+      <GdContainer>
+        <div class="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+          <span class="block sm:inline">Grady Door & Window Manufacturers</span>
+        </div>
+      </GdContainer>
+    </footer>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { setupLayouts } from 'virtual:generated-layouts'
 import './style.css'
 import App from './App.vue'
-import routes from '~pages'
 
-import { createPinia } from 'pinia'
+import generatedRoutes from '~pages'
+
+const routes = setupLayouts(generatedRoutes)
 
 const pinia = createPinia()
 const router = createRouter({
