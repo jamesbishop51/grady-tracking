e9487dac21106be281db3d80e3f0e7159b5f45a8
Shane Gray <shane.joseph.gray@gmail.com>
2023-01-31 12:28:24 +0000

    refactor store with UI errors

--- src/features/operators/operator-store.ts
+++ src/features/operators/operator-store.ts
@@ -23,8 +23,8 @@ export interface SelectOption {
 }
 
 export interface SelectedUser {
-  name?: string
-  id?: string
+  name: string
+  id: string
   task?: string
 }
 
@@ -38,156 +38,68 @@ export interface TaskEntry {
 export const useOperatorStore = defineStore('operator-store', () => {
   /** State */
   const operatorItems = ref<Operators[]>(options)
-  const currentUserId = ref<string>("")
-  const currentUserTask = ref<string>("")
-  const taskEntries = ref<TaskEntry[]>([])
-  const scanTime = ref<Date>()
-  const user = ref<SelectedUser>()
-  const scannedBarcode = ref<string>()
-  /** watcher */
-
-  //
+  const currentUser = ref<SelectedUser>()
 
   /** Getters */
   const currentUserTasks = computed<TaskItems[]>(() => {
-    if (!currentUserId) return []
+    if (!currentUser.value || !currentUser.value.id)
+      return []
 
-    const data = operatorItems.value.find(d => d.id === currentUserId.value)
+    const data = operatorItems.value.find(d => d.id === currentUser.value?.id)
     if (!data) return []
 
     return data.tasks
   })
 
-  const currentUserName = computed(() => selectUsername())
-
-  const selectedUser = computed<SelectedUser>(() => ({
-    name: currentUserName.value,
-    id: currentUserId.value,
-    task: currentUserTask.value
-  }))
-
   const operatorsToOptions = computed<SelectOption[]>(() =>
     operatorItems.value.map(o => ({ text: o.name, value: o.id })),
   )
 
-  const nameAndTask = computed(() => {
-    if (!currentUserTask)
-      return selectedUser.value.name
-
-    return `${user.value?.name} | ${user.value?.task}`
-  })
-
   /** Actions */
-
-  function resetTasks() {
-    taskEntries.value = [{
-      task:"",
-      batchNo:""
-    }]
+  async function selectUserById(id: string) {
+    const name = operatorItems.value.find(d => d.id === id)?.name ?? ''
+    currentUser.value = {
+      id,
+      name
+    }
+    await set("user", JSON.stringify(currentUser.value))
   }
 
-  function selectUserById(id: string) {
-    currentUserId.value = id
+  async function selectTask(task: string) {
+    if (currentUser.value)
+      currentUser.value.task = task
   }
 
-  function selectUsername() {
-    const data = operatorItems.value.find(d => d.id === currentUserId.value)
-    if (!data) return ""
-    return data.name
-  }
-
-  async function loadUser() {
+  async function loadUserLocalUser() {
     const previouslyLoggedIN = await get("user")
     const previousUser = JSON.parse(previouslyLoggedIN)
-    selectUserById(previousUser.id)
-    user.value = previousUser
+    currentUser.value = previousUser
   }
 
-  async function setUser() {
-    if (!currentUserId.value) throw Error("something broke Users")  // replace with proper error later
+  async function loadUsersAndTasks() {
+    const { data } = await axios.get<Operators[]>(`${pingUrl}`)
+    operatorItems.value = data
+  }
 
-    await set("user", selectedUser.value)
-    loadUser()
+  function postBarcode(entry: TaskEntry) {
+    return axios.post(`${pingUrl}`, entry)
   }
 
   async function logOut() {
     await del("user")
   }
 
-  async function completeTask() {
-    if (!currentUserId.value) throw Error("something broke Tasks") // replace with proper error later
-
-    const previousTaskJson = await get<string>(currentUserId.value)
-    const previousTasks = JSON.parse(previousTaskJson || '[]');
-
-    previousTasks.unshift({
-      entries: taskEntries.value,
-      dateSubmitted: new Date()
-    })
-
-    await set(currentUserId.value, JSON.stringify(previousTasks))
-
-    resetTasks()
-
-  }
-
-  async function loadTaskHistory(id: string) {
-    const previousTaskJson = await get<string>(id)
-    const previousTasks = JSON.parse(previousTaskJson || '[]')
-    taskHistory.value = previousTasks;
-  }
-
-
-
-  // async function loadUsersAndTasks() {
-  //   try {
-  //     const { data } = await axios.get(`${pingUrl}`)
-  //     console.log(data)
-  //     message.value = data
-  //     operatorItems.value = data.users
-  //   }
-  //   catch (e: any) {
-  //     console.error(e)
-  //     message.value = `err:${JSON.stringify(e)}`
-
-  //   }
-  // }
-
-  // async function postTest(id: string) {
-  //   try {
-  //     return axios.post(`${pingUrl}/${id}`)
-  //   }
-  //   catch (e: any) {
-  //     console.error(e)
-  //     messagePost.value = `err:${JSON.stringify(e)}`
-  //   }
-  // }
-
   return {
     operatorItems,
-    selectedUser,
-    currentUserName,
-    currentUserTask,
-    currentUserId,
+    currentUser,
     currentUserTasks,
     operatorsToOptions,
-    scanTime,
-    nameAndTask,
-    user,
-    taskEntries,
-    taskHistory,
-    message,
-    scannedBarcode,
-    messagePost,
+    loadUsersAndTasks,
+    loadUserLocalUser,
     selectUserById,
-    completeTask,
-    loadUser,
-    loadTaskHistory,
-    // loadUsersAndTasks,
-    // postTest,
-    setUser,
+    selectTask,
+    postBarcode,
     logOut
-
   }
 });
 
