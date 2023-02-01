85314973a0d6c7927ae14840994713418c7cc0d4
James Bishop <bishbashbosh51@hotmail.com>
2023-02-01 12:11:26 +0000

    commented out buttons for future use

--- src/pages/tasks/[id]/index.vue
+++ src/pages/tasks/[id]/index.vue
@@ -17,8 +17,8 @@ const store = useOperatorStore()
         <h5 v-if="store.currentUser" class="text-2xl font-bold tracking-tight px-4 pt-4">{{ store.currentUser.name }} | {{ store.currentUser.task }}</h5>
         <div class="p-4 grid gap-4 sm:grid-cols-4">
           <GdButtonLink :to="`/tasks/${id}/scan-barcode`">Scan Barcode</GdButtonLink>
-          <GdButtonLink to="/">Error List</GdButtonLink>
-          <GdButtonLink :to="`/tasks/${id}/scan-history/`">Scan History</GdButtonLink>
+          <!-- <GdButtonLink to="/">Error List</GdButtonLink>
+          <GdButtonLink :to="`/tasks/${id}/scan-history/`">Scan History</GdButtonLink> -->
           <GdButtonLink @click="store.logOut" to="/">Change User</GdButtonLink>
         </div>
         
