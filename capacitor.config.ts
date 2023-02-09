c556a21f6f49d2a23f04eb20ced3a53ad01e6dde
james bishop <s00187350@mail.itsligo.ie>
2023-02-09 12:12:33 +0000

    Small QoL Updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,11 @@ const config: CapacitorConfig = {
   appName: 'grady-tracking',
   webDir: 'dist',
   bundledWebRuntime: false,
+  server: {
+    url: "http://100.92.238.70:5173",
+    // url: "http://192.168.1.111:5173",
+    cleartext: true
+  },
 };
 
 export default config;
